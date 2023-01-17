ab9b03c874faf9de3281bcda3b123bae942e07fb
Wesley Pritchard <wespritchard@gmail.com>
2023-01-17 17:01:57 -0500

    split apt and dnf dependency files

--- modules/package_deps_install.py
+++ modules/package_deps_install.py
@@ -3,13 +3,15 @@
 def install_pkg_deps(pkgmgr):
     from os.path import dirname, abspath
     import os
+    from packagemanager_check import packagemanager_check as pm_check
+    pm = pm_check()
     dir_up = dirname(dirname(abspath(__file__)))
-    with open(f"{dir_up}/dependencies/package_dependencies.txt", "r") as pkgs:
+    with open(f"{dir_up}/dependencies/{pm}package_dependencies.txt", "r") as pkgs:
         deps = str(pkgs.read()).replace("\n",' ')    
         if pkgmgr == "apt":
-            os.system(f"sudo {pkgmgr} install {deps} -y")
+            os.system(f"sudo {pkgmgr} install -y {deps}")
         elif pkgmgr == "dnf" or pkgmgr == "yum":
-            os.system(f"sudo {pkgmgr} install {deps} -y")
+            os.system(f"sudo {pkgmgr} install -y {deps}")
 
 if __name__ == "__main__":
     from packagemanager_check import packagemanager_check as pm_check

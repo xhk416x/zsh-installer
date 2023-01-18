0713daaf7984c0aacb244a43e48b1a32a37756a5
xhk416x <68830971+xhk416x@users.noreply.github.com>
2023-01-18 16:29:53 -0500

    corrected module execution
    updated to ensure sequential modules did not execute without prior module successful completion

--- install.sh
+++ install.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-python3 modules/package_deps_install.py
-python3 modules/py_mod_deps.py
-python3 modules/zsh-installer.py
+python3 modules/package_deps_install.py && python3 modules/py_mod_deps.py && python3 modules/zsh-installer.py
